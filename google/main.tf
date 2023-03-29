4fa4724cfa07952aea0e3edd309b8ef8c5552e7a
Artur Janas <artur@infracost.io>
2023-03-29 10:34:06 +0100

    Update main.tf

--- google/main.tf
+++ google/main.tf
@@ -5,7 +5,7 @@ provider "google" {
 
 resource "google_compute_instance" "instance1" {
  name         = "instance1"
- machine_type = "n1-standard-32" # <<<<< Try changing this to n1-standard-8 to compare the costs
+ machine_type = "n1-standard-8" # <<<<< Try changing this to n1-standard-8 to compare the costs
  zone         = "us-central1-a"
 
  boot_disk {
@@ -19,7 +19,7 @@ resource "google_compute_instance" "instance1" {
  }
 
   guest_accelerator {
-    type = "nvidia-tesla-t4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
+    type = "nvidia-tesla-p4" # <<<<< Try changing this to nvidia-tesla-p4 to compare the costs
     count = 4
   }
 
