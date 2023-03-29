091b11fe7f830f8b53fffe0d914c860b2a4c7181
Artur Janas <artur@infracost.io>
2023-03-29 13:55:38 +0100

    Update main.tf

--- google/main.tf
+++ google/main.tf
@@ -47,6 +47,22 @@ resource "aws_instance" "web_app_2" {
   }
 }
 
+resource "aws_instance" "web_app_3" {
+  ami           = "ami-674cbc1e"
+  instance_type = "m7g.4xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+
+  root_block_device {
+    volume_size = 50
+  }
+
+  ebs_block_device {
+    device_name = "my_data"
+    volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
+    volume_size = 500
+    iops        = 800
+  }
+}
+
 resource "google_dns_record_set" "frontend" {
   name = "frontend.123"
   type = "A"
