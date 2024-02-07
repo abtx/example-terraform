93510f7df88cc1fec4f4a82f76a0c68bd24be903
Artur Janas <artur@infracost.io>
2024-02-07 10:03:09 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ provider "aws" {
 
 resource "aws_instance" "web_app" {
   ami           = "ami-674cbc1e"
-  instance_type = "m6.6xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
+  instance_type = "m8.6xlarge"              # <<<<< Try changing this to m5.8xlarge to compare the costs
 
   root_block_device {
     volume_size = 50
@@ -17,7 +17,7 @@ resource "aws_instance" "web_app" {
   ebs_block_device {
     device_name = "my_data"
     volume_type = "io1"                     # <<<<< Try changing this to gp2 to compare costs
-    volume_size = 500
+    volume_size = 800
     iops        = 800
   }
 }
@@ -27,5 +27,5 @@ resource "aws_lambda_function" "hello_world" {
   role          = "arn:aws:lambda:us-east-1:account-id:resource-id"
   handler       = "exports.test"
   runtime       = "nodejs12.x"
-  memory_size   = 1024                      # <<<<< Try changing this to 512 to compare costs
+  memory_size   = 2048                      # <<<<< Try changing this to 512 to compare costs
 }
